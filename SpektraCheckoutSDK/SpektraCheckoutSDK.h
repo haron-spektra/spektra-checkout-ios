43daabb16928ba90415d1fd6d3bceb2d33e5b87e
Haron Ongaki <haron@spektra.co>
2020-04-16 15:01:44 +0300

    Initial Commit

---
+++ SpektraCheckoutSDK/SpektraCheckoutSDK.h
@@ -0,0 +1,19 @@
+//
+//  SpektraCheckoutSDK.h
+//  SpektraCheckoutSDK
+//
+//  Created by Haron Ongaki on 16/04/2020.
+//  Copyright © 2020 Spektra Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpektraCheckoutSDK.
+FOUNDATION_EXPORT double SpektraCheckoutSDKVersionNumber;
+
+//! Project version string for SpektraCheckoutSDK.
+FOUNDATION_EXPORT const unsigned char SpektraCheckoutSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpektraCheckoutSDK/PublicHeader.h>
+
+
